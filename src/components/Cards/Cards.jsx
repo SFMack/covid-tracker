e5947f4efd55f3032dd06d09592b43ab1b6bd372
SFMack <sfcamack@icloud.com>
2020-05-13 00:33:52 -0700

    data is rendering to countup components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import CountUp from "react-countup";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 
-const Cards = props => {
-  console.log(props);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
+
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify='center'>
@@ -12,8 +16,17 @@ const Cards = props => {
             <Typography color='textSecondary' gutterBottom>
               Infected
             </Typography>
-            <Typography variant='h5'>REAL DATA</Typography>
-            <Typography color='textSecondary'>REAL DATE</Typography>
+            <Typography variant='h5'>
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=','
+              />
+            </Typography>
+            <Typography color='textSecondary'>
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant='body2'>
               Number of active cases of COVID-19
             </Typography>
@@ -24,8 +37,17 @@ const Cards = props => {
             <Typography color='textSecondary' gutterBottom>
               Recovered
             </Typography>
-            <Typography variant='h5'>REAL DATA</Typography>
-            <Typography color='textSecondary'>REAL DATE</Typography>
+            <Typography variant='h5'>
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                separator=','
+              />
+            </Typography>
+            <Typography color='textSecondary'>
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant='body2'>
               Number of recoveries from COVID-19
             </Typography>
@@ -36,8 +58,17 @@ const Cards = props => {
             <Typography color='textSecondary' gutterBottom>
               Death
             </Typography>
-            <Typography variant='h5'>REAL DATA</Typography>
-            <Typography color='textSecondary'>REAL DATE</Typography>
+            <Typography variant='h5'>
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=','
+              />
+            </Typography>
+            <Typography color='textSecondary'>
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant='body2'>
               Number of deaths caused by COVID-19
             </Typography>
