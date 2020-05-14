5801fa74c022355f86ae590cbb716fd5e33f5178
SFMack <sfcamack@icloud.com>
2020-05-13 23:12:53 -0700

    covid19 tracker complete

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import { Cards, Chart, CountryPicker } from "./components/index";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
+import covidLogo from "./images/covidLogo.png";
 
 export default class App extends Component {
   state = {
     data: {},
+    country: "",
   };
 
   async componentDidMount() {
@@ -13,13 +15,19 @@ export default class App extends Component {
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async country => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={covidLogo} alt='covid 19 logo' />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
